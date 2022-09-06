1dbd23844ec612ca9e6a45bcc5cbc0ad40662c9f
azlinliana <azlinliana.adlan@gmail.com>
2022-09-06 17:22:31 +0800

    Enable email verification and reset password when forgot in Breeze features using Mailhog

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,9 @@
 @props(['status'])
 
 @if ($status)
+  <div class="bg-green-100 rounded-lg py-5 px-6 mb-4 text-base mb-3" role="alert">
     <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
-        {{ $status }}
+      {{ $status }}
     </div>
+  </div>
 @endif
